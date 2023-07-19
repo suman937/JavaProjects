5dd8d4dc1787e34b9f6ed7c597cd14e2fedc4216
Suman Lamichhane <sumanlamichhane@Sumans-MBP.lan>
2023-07-19 04:51:15 -0400

    first commit of OOP aplication

---
+++ src/Car.java
@@ -0,0 +1,15 @@
+
+public class Car {
+
+	String make = "Chevrolet";
+	String model = "Corvett";
+	String color = "blue";
+	double price = 50000.00;
+	
+	void drive() {
+		System.out.println("you drive the car");
+	}
+	void brake() {
+		System.out.println("you press on the break");
+	}
+}
---
+++ src/Main.java
@@ -0,0 +1,23 @@
+
+public class Main {
+
+	public static void main(String[]args) {
+		
+		//object = an instance of a class that may contain attributes and methods
+		//example: (phone, desk , computer, coffee cup)
+		
+		Car mycar1 = new Car();
+		Car mycar2 = new Car();
+		
+		System.out.println(mycar1.model);
+		System.out.println(mycar1.make);
+		
+		System.out.println();
+		System.out.println(mycar2.model);
+		System.out.println(mycar2.make);
+		System.out.println("Git commit test");
+		
+//		mycar.drive();
+//		mycar.brake();
+	}
+}
